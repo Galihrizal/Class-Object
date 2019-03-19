63c47d820a24d53d01311f7b4942680cadba60cf
Galihrizal <galih.2301@students.amikom.ac.id>
2019-03-19 14:52:39 +0700

    Class & Object bagian 1

---
+++ Mahasiswa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace class1
+{
+    class Mahasiswa
+    {
+        //properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        //method
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM : {0}", Nim);
+            Console.WriteLine("IPK : {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void IsiKrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses Krs\n", Nama);   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace class1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            //mengeset nilai properties objek saya dan kamu
+            saya.Nim = "12345";
+            saya.Nama = "Jono";
+            saya.Ipk = 3.95f;
+
+            kamu.Nim = "12346";
+            kamu.Nama = "Paijo";
+            kamu.Ipk = 3.70f;
+
+            //memanggil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
